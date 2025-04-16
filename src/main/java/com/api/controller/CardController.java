e9a7c8bc76c608700c1b687fbb98c448bfb3c004
jwujesq8 <zhukovskaja.elena@gmail.com>
2025-04-16 19:33:48 +0200

    added @Operation to controllers

--- src/main/java/com/api/controller/CardController.java
+++ src/main/java/com/api/controller/CardController.java
@@ -2,6 +2,7 @@ package com.api.controller;
 
 import com.api.dto.*;
 import com.api.service.interfaces.CardService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -18,6 +19,7 @@ public class CardController {
 
     private final CardService cardService;
 
+    @Operation(summary = "get card by id - only for admin and card owner")
     @PostMapping
     @PreAuthorize("isAuthenticated() && " +
             "(hasRole('ADMIN') || @permissionChecker.isCardOwner(#cardIdDto, authentication.principal))")
@@ -25,6 +27,7 @@ public class CardController {
         return ResponseEntity.ok(cardService.getCardById(cardIdDto.getId()));
     }
 
+    @Operation(summary = "add a new card - only for admin")
     @PostMapping("/new")
     @PreAuthorize("isAuthenticated() && hasRole('ADMIN')")
     public ResponseEntity<CardDto> addCard(@RequestBody @Valid CardDtoNoId cardDtoNoId){
@@ -33,18 +36,21 @@ public class CardController {
                 .body(cardService.addCard(cardDtoNoId));
     }
 
+    @Operation(summary = "update card that already exists in the database - only for admin")
     @PutMapping
     @PreAuthorize("isAuthenticated() && hasRole('ADMIN')")
     public ResponseEntity<CardDto> updateCard(@RequestBody @Valid CardDto cardDto) {
         return ResponseEntity.ok(cardService.updateCard(cardDto));
     }
 
+    @Operation(summary = "update only cards status - only for admin")
     @PutMapping("/status")
     @PreAuthorize("isAuthenticated() && hasRole('ADMIN')")
     public void updateCardStatus(@RequestBody @Valid CardIdStatusDto cardIdStatusDto){
         cardService.updateCardStatus(cardIdStatusDto.getId(), cardIdStatusDto.getNewStatus().name());
     }
 
+    @Operation(summary = "update only cards transaction limit per date - only for admin")
     @PutMapping("/transactionLimitPerDay")
     @PreAuthorize("isAuthenticated() && hasRole('ADMIN')")
     public void updateCardsTransactionLimitPerDayById(@RequestBody @Valid CardIdLimitDto cardIdLimitDto){
@@ -52,12 +58,14 @@ public class CardController {
                 cardIdLimitDto.getNewTransactionLimitPerDay());
     }
 
+    @Operation(summary = "delete card by id - only for admin")
     @DeleteMapping
     @PreAuthorize("isAuthenticated() && hasRole('ADMIN')")
     public void deleteCardById(@RequestBody @Valid IdDto cardIdDto){
         cardService.deleteCardById(cardIdDto.getId());
     }
 
+    @Operation(summary = "get all cards (paging) - only for admin")
     @GetMapping("/all")
     @PreAuthorize("isAuthenticated() && hasRole('ADMIN')")
     public ResponseEntity<Page<CardDto>> findAll(@RequestParam(defaultValue = "0") int page,
@@ -65,6 +73,7 @@ public class CardController {
         return ResponseEntity.ok(cardService.findAll(PageRequest.of(page, size)));
     }
 
+    @Operation(summary = "get all cards by card owner id (paging) - only for admin and cards owner")
     @GetMapping("/all/owner")
     @PreAuthorize("isAuthenticated() && " +
             "(hasRole('ADMIN') || @permissionChecker.isOwnerRequestToFindAllHisCards(#ownerIdDto, authentication.principal))")
--- src/main/java/com/api/controller/TransactionController.java
+++ src/main/java/com/api/controller/TransactionController.java
@@ -5,6 +5,7 @@ import com.api.dto.PaymentDto;
 import com.api.dto.TransactionDto;
 import com.api.dto.TransactionDtoNoId;
 import com.api.service.interfaces.TransactionService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -21,6 +22,7 @@ public class TransactionController {
 
     private final TransactionService transactionService;
 
+    @Operation(summary = "get transaction by id - only for admin and source or destination card owner")
     @PostMapping
     @PreAuthorize("isAuthenticated() && " +
             "(hasRole('ADMIN') || " +
@@ -29,6 +31,7 @@ public class TransactionController {
         return ResponseEntity.ok(transactionService.getTransactionById(transactionIdDto.getId()));
     }
 
+    @Operation(summary = "add new transaction - only for admin")
     @PostMapping("/new")
     @PreAuthorize("isAuthenticated() && hasRole('ADMIN')")
     public ResponseEntity<TransactionDto> addTransaction(@RequestBody @Valid TransactionDtoNoId transactionDtoNoId){
@@ -37,24 +40,28 @@ public class TransactionController {
                 .body(transactionService.addTransaction(transactionDtoNoId));
     }
 
+    @Operation(summary = "update transaction that already exists in the database - only for admin")
     @PutMapping
     @PreAuthorize("isAuthenticated() && hasRole('ADMIN')")
     public ResponseEntity<TransactionDto> updateTransaction(@RequestBody @Valid TransactionDto transactionDto){
         return ResponseEntity.ok(transactionService.updateTransaction(transactionDto));
     }
 
+    @Operation(summary = "delete transaction by id - only for admin")
     @DeleteMapping
     @PreAuthorize("isAuthenticated() && hasRole('ADMIN')")
     public void deleteTransactionById(@RequestBody @Valid IdDto transactionIdDto){
         transactionService.deleteTransactionById(transactionIdDto.getId());
     }
 
+    @Operation(summary = "make a transaction - only for a source card owner")
     @PostMapping("/make")
     @PreAuthorize("isAuthenticated() && @permissionChecker.isSourceCardOwnerRequestToMakeTransaction(#paymentDto, authentication.principal)")
     public void makeTransaction(@RequestBody @Valid PaymentDto paymentDto){
         transactionService.makeTransaction(paymentDto.getSourceCardId(), paymentDto.getDestinationCardId(), paymentDto.getAmount());
     }
 
+    @Operation(summary = "get all transactions (paging) - only for admin")
     @GetMapping("/all")
     @PreAuthorize("isAuthenticated() && hasRole('ADMIN')")
     public ResponseEntity<Page<TransactionDto>> findAll(@RequestParam(defaultValue = "0") int page,
@@ -62,6 +69,7 @@ public class TransactionController {
         return ResponseEntity.ok(transactionService.findAll(PageRequest.of(page, size)));
     }
 
+    @Operation(summary = "get all transaction by source or destination card id (paging) - only for admin and card owner")
     @PostMapping("/all/card")
     @PreAuthorize("isAuthenticated() && " +
             "(hasRole('ADMIN') || " +
