2610d7311e859c64ee7f7ad11efec360fdbcebad
jwujesq8 <zhukovskaja.elena@gmail.com>
2025-05-07 10:20:46 +0200

    auth controller test

--- src/main/java/com/api/dto/jwt/JwtRequestDto.java
+++ src/main/java/com/api/dto/jwt/JwtRequestDto.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,6 +18,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
+@Builder
 public class JwtRequestDto {
 
     /**
